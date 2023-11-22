15629584124b8c55020ceb2ef37c1c74ed2a49c2
Dawid Niezgódka <dawid.niezgodka@gmail.com>
2023-11-22 09:58:37 +0100

    further test

--- src/comment.js
+++ src/comment.js
@@ -186,10 +186,10 @@ module.exports.createBodyForComparisonWithTrendDetDeltas = function(evaluationRe
       console.log(`No benchmark values found for metric: ${metricName}`);
       continue;
     }
-    const currBenchValue = metricValues ? metricValues.get('current') ?? 'N/A' : 'N/A';
-    const prevBenchValue = metricValues ? metricValues.get('previous') ?? 'N/A' : 'N/A';
-    const weekAgoBenchValue = metricValues ? metricValues.get('week_ago') ?? 'N/A' : 'N/A';
-    const lastStableReleaseBenchValue = metricValues ? metricValues.get('last_stable_release') ?? 'N/A' : 'N/A';
+    const currBenchValue = metricValues.get('current') ? metricValues.get('current') : 'N/A';
+    const prevBenchValue = metricValues.get('previous') ? metricValues.get('previous') : 'N/A';
+    const weekAgoBenchValue = metricValues.get('week_ago') ? metricValues.get('week_ago') : 'N/A';
+    const lastStableReleaseBenchValue = metricValues.get('last_stable_release') ? metricValues.get('last_stable_release') : 'N/A';
 
 
     const x = evaluationConfiguration.trendThresholds[i];
